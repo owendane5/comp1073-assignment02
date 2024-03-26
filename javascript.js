d703718286aff759f2d3bf6ae5e26719211bda1a
owendane5 <112992614+owendane5@users.noreply.github.com>
2024-03-26 16:11:40 -0400

    starting display method

--- javascript.js
+++ javascript.js
@@ -1,11 +1,27 @@
 //owen dane
-
-
+const orderSummary = document.getElementById('orderSum')
+//creating smoothie class
 class Smoothie{
     constructor(smoothieType, milkType, smoothieOption, additionalRequest){
         this.smoothieType = smoothieType;
         this.milkType = milkType;
         this.smoothieOption = smoothieOption;
         this.additionalRequest = additionalRequest;
+    }
+    //printing the smoothie details inside the 
+    displayOrder() {
+        //clearing the texts
+        orderSummary.textContent = '';
+        //elements for smoothie!
+        let smoothieTypeHead = document.createElement('h3');
+        let milkTypeText = document.createTextNode(`Type of Milk: ${this.milkType}`);
+        let smoothieOptionImg = document.createElement('img');
+        let additionalRequestText = document.createTextNode(`Additional Requests: ${this.additionalRequest}`);
+        //throwing in the ternary
+        smoothieTypeHead.textContent = `Flavour: ${this.smoothieType}`;
+        smoothieOptionImg.src = this.smoothieOption === 'smoothie' ? 'images/bowl.jpg' : 'images/cup.jpg';
+
+      
+        
     }
 }
