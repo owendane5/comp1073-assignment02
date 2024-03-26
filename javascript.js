feb15981ef263613d76e9352e68f71a69dfea728
owendane5 <112992614+owendane5@users.noreply.github.com>
2024-03-26 15:37:49 -0400

    added smoothie class and few changes to html

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-let owen;
+//owen dane
+
+
+class Smoothie{
+    constructor(smoothieType, milkType, smoothieOption, additionalRequest){
+        this.smoothieType = smoothieType;
+        this.milkType = milkType;
+        this.smoothieOption = smoothieOption;
+        this.additionalRequest = additionalRequest;
+    }
+}
